61549b883b4a6060c1df0b060bd70f3ea9aed38c
Josef Kranzer <kranzerj@gmail.com>
2025-11-14 12:50:47 +0100

    Delete update_oem_firmware_client/hp directory

--- update_oem_firmware_client/hp/02_install_ImageAssistant_via_cmsl.ps1
+++
@@ -1,6 +0,0 @@
-#requires cmsl (https://github.com/kranzerj/Intune/blob/main/update_oem_firmware_client/hp/01_install_cmsl.ps1       https://developers.hp.com/hp-client-management) 
-#Detection rule: File: C:\HPI\HPImageAssistant.exe exists
-#uninstall Command: powershell .....: Remove-Item C:\HPI -Recurse
-
-
-Install-HPImageAssistant -Extract C:\HPI -Quiet
